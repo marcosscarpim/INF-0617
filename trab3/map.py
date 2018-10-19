a965f1f3c103b023e2fbdc6b0ed72a1390941c14
mscarpim <mscarpim@motorola.com>
2018-10-19 15:50:40 -0300

    trab3 working

---
+++ trab3/map.py
@@ -0,0 +1,22 @@
+import sys
+import re
+import io
+
+
+with io.open(sys.stdin.fileno(), 'r', encoding='latin-1') as sin:
+    i = 0
+    for line in sin:
+        fields = line.split(";")
+
+        # remove votos que não sejam pra governador
+        if "GOVERNADOR" not in fields[12]:
+            continue
+        # remove votos que não sejam de 2014
+        if "2014" not in fields[2]:
+            continue
+
+        num_cand = fields[13].replace("\"", "")
+        qtde_votos = fields[14].replace("\n", "").replace("\"", "")
+
+        print(num_cand+","+qtde_votos)
+
---
+++ trab3/reduce.py
@@ -0,0 +1,21 @@
+import sys
+
+num_votos = {}
+for line in sys.stdin:
+    num_cand, qtde_votos = line.split(",")
+    num_cand = num_cand
+    qtde_votos = int(qtde_votos)
+
+    if num_cand in num_votos.keys():
+        num_votos[num_cand] += qtde_votos
+    else:
+        num_votos[num_cand] = qtde_votos
+
+for cand in num_votos.keys():
+    if cand == "95":
+        print("VOTOS BRANCOS = " + str(num_votos[cand]))
+    elif cand == "96":
+        print("VOTOS NULOS = " + str(num_votos[cand]))
+    else:
+        print("CANDIDATO = " + cand + ", VOTOS = " + str(num_votos[cand]))
+
