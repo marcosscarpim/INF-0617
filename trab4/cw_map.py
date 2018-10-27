d5d31cb36cd3b59606326178b6661ed8b187ec83
Liselene de Abreu Borges <liselene@vntnotl7914.venturus.local>
2018-10-26 23:44:56 -0300

    word count map

--- trab4/cw_map.py
+++ trab4/cw_map.py
@@ -1,30 +1,25 @@
 import os
 import re
 import time
-from multiprocessing import Process
 
 # fix this in your computer
 os.chdir("/home/liselene/Projects/ComplexData/INF-0617/trab4/gut_test/")
-print(os.getcwd())
+#print(os.getcwd())
 
 def count_words(content):
+    unique_words = set(content)
+
+    for word in unique_words:
+        count = content.count(word)
+        print(word,',',count)
+
+def read_words(content):
     content = content.lower().replace('\n', ' ')
     p = re.compile('[0-9]')
     content=p.sub('',content)
     p = re.compile(r'\W+')
     words = p.split(content)
-
-    unique_words = set(words)
-    print(unique_words)
-    
-#print(content)
-
-#    for char in unique_chars:
-#        count = content.count(word)
-#        if count > final_count:
-#            letter = char
-#            final_count = count
-#    return letter, final_count
+    return words
 
 filenames = []
 
@@ -35,7 +30,6 @@ for file in os.listdir("./txt"):
 for filename in filenames:
     f = open("./txt/" + filename, encoding="utf-8", errors="ignore")
     content = f.read()
-    count_words(content)
+    words = read_words(content)
     f.close()
-    print(filename)
-
+    count_words(words)
